c48448ae7f74544d219448fbb167e13e751dfaf3
deakjanos <deaak.jaanos@gmail.com>
2013-02-03 15:41:47 +0100

    Szavak

---
+++ Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.maganhangzovizsgalatTextBox = New System.Windows.Forms.TextBox
+        Me.maganhangzovizsgalatButton = New System.Windows.Forms.Button
+        Me.maganhangzovalaszTextBox = New System.Windows.Forms.TextBox
+        Me.beolvasButton = New System.Windows.Forms.Button
+        Me.beolvasottTextBox = New System.Windows.Forms.TextBox
+        Me.leghosszabbTextBox = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.katakterekszamaTextBox = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Adjon meg egy szót:"
+        '
+        'maganhangzovizsgalatTextBox
+        '
+        Me.maganhangzovizsgalatTextBox.Location = New System.Drawing.Point(120, 12)
+        Me.maganhangzovizsgalatTextBox.Name = "maganhangzovizsgalatTextBox"
+        Me.maganhangzovizsgalatTextBox.Size = New System.Drawing.Size(437, 20)
+        Me.maganhangzovizsgalatTextBox.TabIndex = 1
+        '
+        'maganhangzovizsgalatButton
+        '
+        Me.maganhangzovizsgalatButton.Location = New System.Drawing.Point(15, 44)
+        Me.maganhangzovizsgalatButton.Name = "maganhangzovizsgalatButton"
+        Me.maganhangzovizsgalatButton.Size = New System.Drawing.Size(283, 23)
+        Me.maganhangzovizsgalatButton.TabIndex = 2
+        Me.maganhangzovizsgalatButton.Text = "Van-e magánhagzó benne?"
+        Me.maganhangzovizsgalatButton.UseVisualStyleBackColor = True
+        '
+        'maganhangzovalaszTextBox
+        '
+        Me.maganhangzovalaszTextBox.Location = New System.Drawing.Point(304, 46)
+        Me.maganhangzovalaszTextBox.Name = "maganhangzovalaszTextBox"
+        Me.maganhangzovalaszTextBox.Size = New System.Drawing.Size(252, 20)
+        Me.maganhangzovalaszTextBox.TabIndex = 3
+        '
+        'beolvasButton
+        '
+        Me.beolvasButton.Location = New System.Drawing.Point(12, 91)
+        Me.beolvasButton.Name = "beolvasButton"
+        Me.beolvasButton.Size = New System.Drawing.Size(112, 30)
+        Me.beolvasButton.TabIndex = 4
+        Me.beolvasButton.Text = "Beolvasás"
+        Me.beolvasButton.UseVisualStyleBackColor = True
+        '
+        'beolvasottTextBox
+        '
+        Me.beolvasottTextBox.Location = New System.Drawing.Point(130, 91)
+        Me.beolvasottTextBox.Multiline = True
+        Me.beolvasottTextBox.Name = "beolvasottTextBox"
+        Me.beolvasottTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.beolvasottTextBox.Size = New System.Drawing.Size(167, 74)
+        Me.beolvasottTextBox.TabIndex = 5
+        '
+        'leghosszabbTextBox
+        '
+        Me.leghosszabbTextBox.Location = New System.Drawing.Point(311, 118)
+        Me.leghosszabbTextBox.Name = "leghosszabbTextBox"
+        Me.leghosszabbTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.leghosszabbTextBox.Size = New System.Drawing.Size(245, 20)
+        Me.leghosszabbTextBox.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(312, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "A leghosszabb szó:"
+        '
+        'katakterekszamaTextBox
+        '
+        Me.katakterekszamaTextBox.Location = New System.Drawing.Point(513, 145)
+        Me.katakterekszamaTextBox.Name = "katakterekszamaTextBox"
+        Me.katakterekszamaTextBox.Size = New System.Drawing.Size(44, 20)
+        Me.katakterekszamaTextBox.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(311, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(199, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "A leghosszabb szó karaktereinek száma:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(569, 429)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.katakterekszamaTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.leghosszabbTextBox)
+        Me.Controls.Add(Me.beolvasottTextBox)
+        Me.Controls.Add(Me.beolvasButton)
+        Me.Controls.Add(Me.maganhangzovalaszTextBox)
+        Me.Controls.Add(Me.maganhangzovizsgalatButton)
+        Me.Controls.Add(Me.maganhangzovizsgalatTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents maganhangzovizsgalatTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents maganhangzovizsgalatButton As System.Windows.Forms.Button
+    Friend WithEvents maganhangzovalaszTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents beolvasButton As System.Windows.Forms.Button
+    Friend WithEvents beolvasottTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents leghosszabbTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents katakterekszamaTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports System
+Imports System.IO
+
+
+Public Class Form1
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub maganhangzovizsgalatButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles maganhangzovizsgalatButton.Click
+        Dim mgh() As Char = {"a", "e", "i", "o", "u"}
+        Dim beirt As String
+        Dim beirtkaraktertomb() As Char
+        Dim beirtkarakterszam As Integer
+        Dim i, j As Integer
+        Dim nincs As Boolean
+
+        beirt = maganhangzovizsgalatTextBox.Text
+        beirtkaraktertomb = beirt.ToCharArray
+        beirtkarakterszam = beirtkaraktertomb.Length
+
+        'Console.Write(beirtkarakterszam)
+        'Console.Write(mgh(j))
+        'Console.Write(beirtkaraktertomb(0))
+        nincs = True
+        For j = 0 To 4
+
+
+            For i = 0 To beirtkarakterszam - 1
+
+                If mgh(j) = beirtkaraktertomb(i) Then
+                    maganhangzovalaszTextBox.Text = "Van benne magánhangzó"
+                    Console.Write(mgh(j))
+                    nincs = False
+
+                End If
+            Next
+
+        Next
+
+        If nincs = True Then
+            maganhangzovalaszTextBox.Text = "Nincs benne magánhangzó"
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub beolvasButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles beolvasButton.Click
+        Dim FILE_NAME As String = "szoveg.txt"
+        Dim TextLine As String
+        TextLine = ""
+        Dim szavaktomb() As String
+        Dim cseresztring As String
+        Dim sorszamlalo As Long
+        sorszamlalo = 0
+
+        Dim beolvasottsor As String
+        beolvasottsor = ""
+        Dim beolvasottsor_leghosszabb As String
+        beolvasottsor_leghosszabb = ""
+        Dim hossz As Integer
+        Dim elozohossz As Integer
+        Dim legnagyobb_hossz As Integer
+
+
+        If System.IO.File.Exists(FILE_NAME) = True Then
+
+            Dim objReader1 As New System.IO.StreamReader(FILE_NAME)
+
+            Do While objReader1.Peek() <> -1
+
+                beolvasottsor = objReader1.ReadLine()
+
+                sorszamlalo = sorszamlalo + 1
+
+
+                'Console.Write(sorszamlalo & vbNewLine)
+
+            Loop
+
+            ReDim szavaktomb(sorszamlalo)
+
+
+            Dim objReader As New System.IO.StreamReader(FILE_NAME)
+            sorszamlalo = 0
+
+            Do While objReader.Peek() <> -1
+
+                elozohossz = hossz
+
+                'beolvasottsor_elozo = beolvasottsor
+                beolvasottsor = objReader.ReadLine()
+                szavaktomb(sorszamlalo) = beolvasottsor
+
+
+                hossz = beolvasottsor.Length
+
+                
+
+
+                TextLine = TextLine & CStr(sorszamlalo) & Chr(32) & CStr(hossz) & vbNewLine & beolvasottsor & vbNewLine
+                sorszamlalo = sorszamlalo + 1
+
+            Loop
+
+            'leghosszabbTextBox.Text = beolvasottsor_leghosszabb
+            beolvasottTextBox.Text = TextLine
+
+            Console.Write(szavaktomb(0) & vbNewLine)
+            Console.Write(szavaktomb(1) & vbNewLine)
+            Console.Write(szavaktomb(2) & vbNewLine)
+            Console.Write(szavaktomb(3) & vbNewLine)
+
+
+            'ciklus i=1-től n-1-ig
+            '   ciklus j=i+1-tıl n-ig
+            '       ha x(i)>x(j) akkor
+            '           csere: x(i), x(j)
+            '       elágazás(vége)
+            '   ciklus(vége)
+            'ciklus(vége)
+
+            For i = 1 To sorszamlalo - 2
+                For j = i + 1 To sorszamlalo - 1
+                    If szavaktomb(i).Length > szavaktomb(j).Length Then
+                        cseresztring = szavaktomb(i)
+                        szavaktomb(i) = szavaktomb(j)
+                        szavaktomb(j) = cseresztring
+
+                    End If
+                Next
+            Next
+
+            Console.Write(szavaktomb(sorszamlalo - 4) & vbNewLine)
+            Console.Write(szavaktomb(sorszamlalo - 3) & vbNewLine)
+            Console.Write(szavaktomb(sorszamlalo - 2) & vbNewLine)
+            Console.Write(szavaktomb(sorszamlalo - 1) & vbNewLine)
+
+
+            leghosszabbTextBox.Text = szavaktomb(sorszamlalo - 1)
+            katakterekszamaTextBox.Text = CStr(szavaktomb(sorszamlalo - 1).Length)
+
+
+            
+
+        End If
+
+
+
+
+
+
+    End Sub
+
+    
+    
+    Private Sub tesztTextBox_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+End Class
